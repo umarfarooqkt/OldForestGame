d54fa2d5cbd3fa15ae46f46117e62e6515aa6e2e
umarfarooqkt <umarfarooqkt@gmail.com>
2017-11-13 02:01:50 -0500

    just added the enemies walking script

---
+++ Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	public int target =0;
+	public Transform exitPoint;// for tthe exit from the game, hopefully player cant exit
+	public Transform[] waypoints; // this is actually the reference check points we are going to use to move our enemy
+	public float navigationUpdate; // for 
+
+	private Transform enemy; // gonna be the enemys movement
+	private float navigationTime = 0; 
+	// Use this for initialization
+	void Start () {
+		enemy = GetComponent<Transform>();
+		//get componets lets us address the properties of tranform 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(waypoints != null) {
+			// keeping track since time past since navigation tiem started 
+			navigationTime= navigationTime + Time.deltaTime;
+			if(navigationTime > navigationUpdate){
+				if(target < waypoints.Length){
+					enemy.position =Vector2.MoveTowards(enemy.position, waypoints[target].position, navigationTime);
+				}
+				else{
+					enemy.position = Vector2.MoveTowards(enemy.position,exitPoint.position,navigationTime);
+				}
+				navigationTime=0;
+			}
+		}
+	}
+	// so this is for when we run into our colliders as in the reference points I beleive
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Checkpoint"){
+			target +=1;
+		}
+		// so if the enemy runs into the finish as in exit it will be destroyed boi
+		else if (other.tag == "Finish"){
+			GameManager.instance.removeEnemyFromScreen(); // basically by destroy i mean taek of screen
+			Destroy(gameObject);// and now destroy it
+		} 
+	}
+}
---
+++ Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    /// initializing some game objects
+    public static GameManager instance = null;
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+    public int MaxEnemiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerspawn;
+
+    private int enemiesOnScreen = 0;
+
+	void Awake(){
+		if (instance== null){
+			instance = this;
+		}
+		else if (instance != this){
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		spawnEnemy();
+	}
+	//method to spawn/.creat enemies
+	void spawnEnemy(){
+		if (enemiesPerspawn > 0 && enemiesOnScreen < totalEnemies)
+		{
+			for(int i=0; i < enemiesPerspawn; i++){
+				if(enemiesOnScreen < MaxEnemiesOnScreen){
+					GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+					//setting the enemy position to spawn point position
+					newEnemy.transform.position = spawnPoint.transform.position;
+					enemiesOnScreen+=1; 
+				}
+			}
+		}
+	}
+
+	//method for decrementing enemies on screen
+	public void removeEnemyFromScreen(){
+		if(enemiesOnScreen > 0){ enemiesOnScreen-=1;}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
